e0b1df5b609e3e00d4db5d2b899702dee5cbea7c
Khoa Vu <Khoavu1110@gmail.com>
2025-02-28 16:17:53 -0500

    Adding more stuffs to readme.md

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(){
     Restaurant restaurant;
-    restaurant.openRestaurant(2, 5);
+    restaurant.openRestaurant(5, 5);
     std::this_thread::sleep_for(std::chrono::seconds(5));
     restaurant.closeRestaurant();
     return 0;
